9b24b60305bc3953bdbd8d67d6a9df084c063828
Ajikaran Kamalanathan <ajidev1224@gmail.com>
2023-03-29 10:28:35 +0530

    Create new methods in userDAO repository

--- BackEnd/loan-offer-system/src/main/java/com/dao/impl/UserDAOImpl.java
+++ BackEnd/loan-offer-system/src/main/java/com/dao/impl/UserDAOImpl.java
@@ -2,18 +2,22 @@ package com.dao.impl;
 
 import com.dao.IUserDAO;
 import com.dao.OfferDAOConstant;
+import com.dto.request.UserLoginReq;
+import com.dto.response.CommonResponse;
 import com.dto.response.GeneralResponse;
+import com.dto.response.UserLoginRes;
 import com.dto.user.request.CreateNewUserReq;
+import com.dto.user.request.GetCustomerDetailReq;
+import com.dto.user.response.CustomerRes;
 import com.util.HashUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.stereotype.Repository;
 
-import java.sql.CallableStatement;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Types;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
 @Repository
@@ -63,6 +67,124 @@ public class UserDAOImpl implements IUserDAO {
         }finally {
             DataSourceUtils.releaseConnection(connection,jdbcTemplate.getDataSource());
             logger.info("Time taken for createNewUser in seconds: "+ (double)(System.currentTimeMillis() - startTime)/1000 );
+        }
+        return response;
+    }
+
+    @Override
+    public CustomerRes getCustomerDetail(GetCustomerDetailReq getCustomerDetailReq) {
+        Long startTime = System.currentTimeMillis();
+        CustomerRes response = null;
+        Connection connection = null;
+        CallableStatement callableStatement;
+        ResultSet resultSet;
+        try {
+            logger.info("getCustomerDetail-request------------------>"+getCustomerDetailReq.toString());
+
+            connection = DataSourceUtils.getConnection(jdbcTemplate.getDataSource());
+            callableStatement = connection.prepareCall(OfferDAOConstant.UserConstant.GET_CUSTOMER_DETAIL);
+            callableStatement.setObject(1,getCustomerDetailReq.getCustomerId(),Types.INTEGER);
+            callableStatement.execute();
+            resultSet = callableStatement.getResultSet();
+            if(resultSet != null){
+                response = new CustomerRes();
+                while (resultSet.next()){
+                    response.setFullName(resultSet.getString(1));
+                    response.setDob(resultSet.getString(2));
+                    response.setUserTypeStr(resultSet.getString(3));
+                    response.setUserEmail(resultSet.getString(4));
+                    response.setUserMobileNumber(resultSet.getString(5));
+                    response.setLoanBalance(resultSet.getDouble(6));
+                    response.setUsedAmount(resultSet.getDouble(7));
+                    response.setPlanName(resultSet.getString(8));
+                }
+            }
+        }catch (SQLException exception){
+            logger.info("An error occured in getCustomerDetail "+exception.toString());
+        }finally {
+            DataSourceUtils.releaseConnection(connection,jdbcTemplate.getDataSource());
+            logger.info("Time taken for getCustomerDetail in seconds: "+ (double)(System.currentTimeMillis() - startTime)/1000 );
+        }
+        return response;
+    }
+
+    @Override
+    public List<CustomerRes> getCustomerList() {
+        Long startTime = System.currentTimeMillis();
+        List<CustomerRes> list = null;
+        Connection connection = null;
+        CallableStatement callableStatement;
+        ResultSet resultSet;
+        try {
+            connection = DataSourceUtils.getConnection(jdbcTemplate.getDataSource());
+            callableStatement = connection.prepareCall(OfferDAOConstant.UserConstant.GET_CUSTOMER_LIST);
+            callableStatement.execute();
+            resultSet = callableStatement.getResultSet();
+            if(resultSet != null){
+                list = new ArrayList<>();
+                while (resultSet.next()){
+                    CustomerRes response = new CustomerRes();
+                    response.setFullName(resultSet.getString(1));
+                    response.setDob(resultSet.getString(2));
+                    response.setUserTypeStr(resultSet.getString(3));
+                    response.setUserEmail(resultSet.getString(4));
+                    response.setUserMobileNumber(resultSet.getString(5));
+                    response.setLoanBalance(resultSet.getDouble(6));
+                    response.setUsedAmount(resultSet.getDouble(7));
+                    response.setPlanName(resultSet.getString(8));
+
+                    list.add(response);
+                }
+            }
+        }catch (SQLException exception){
+            logger.info("An error occured in getCustomerList "+exception.toString());
+        }finally {
+            DataSourceUtils.releaseConnection(connection,jdbcTemplate.getDataSource());
+            logger.info("Time taken for getCustomerList in seconds: "+ (double)(System.currentTimeMillis() - startTime)/1000 );
+        }
+        return list;
+    }
+
+    @Override
+    public CommonResponse login(UserLoginReq loginReq) {
+        Long startTime = System.currentTimeMillis();
+        CommonResponse response = new CommonResponse();
+        Connection connection = null;
+        CallableStatement callableStatement;
+        try {
+            logger.info("login-request------------------>"+loginReq.toString());
+
+            connection = DataSourceUtils.getConnection(jdbcTemplate.getDataSource());
+            callableStatement = connection.prepareCall(OfferDAOConstant.UserConstant.LOGIN_USER);
+            callableStatement.setObject(1,loginReq.getUsername(),Types.VARCHAR);
+            callableStatement.setObject(2,HashUtil.getHashCode(loginReq.getPassword()),Types.VARCHAR);
+            callableStatement.registerOutParameter(3,Types.INTEGER);
+            callableStatement.registerOutParameter(4,Types.INTEGER);
+            callableStatement.registerOutParameter(5,Types.VARCHAR);
+            callableStatement.registerOutParameter(6,Types.VARCHAR);
+            callableStatement.registerOutParameter(7,Types.BOOLEAN);
+            callableStatement.registerOutParameter(8,Types.INTEGER);
+            callableStatement.registerOutParameter(9,Types.VARCHAR);
+            callableStatement.execute();
+            callableStatement.getResultSet();
+
+            response.setRes((Boolean) callableStatement.getObject(7));
+            response.setStatusCode((Integer) callableStatement.getObject(8));
+            response.setMsg((String) callableStatement.getObject(9));
+            if(response.isRes()){
+                UserLoginRes loginRes = new UserLoginRes();
+                loginRes.setFirstName( callableStatement.getString(3));
+                loginRes.setLastName( callableStatement.getString(4));
+                loginRes.setUserType( callableStatement.getInt(5));
+                loginRes.setUserId( callableStatement.getInt(6));
+
+                response.setValue(loginRes);
+            }
+        }catch (SQLException exception){
+            logger.info("An error occured in login "+exception.toString());
+        }finally {
+            DataSourceUtils.releaseConnection(connection,jdbcTemplate.getDataSource());
+            logger.info("Time taken for login in seconds: "+ (double)(System.currentTimeMillis() - startTime)/1000 );
         }
         return response;
     }
